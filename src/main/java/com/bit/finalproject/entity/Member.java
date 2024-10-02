6d1fa2e8ee36de6cfac3a11706f16c264b4f7b3c
COM\planl <bobaesj20@naver.com>
2024-10-02 16:13:17 +0900

    resource파일 추가

--- src/main/java/com/bit/finalproject/entity/Member.java
+++ src/main/java/com/bit/finalproject/entity/Member.java
@@ -48,8 +48,8 @@ public class Member {
     private UserStatus userStatus;
 
     // 프로필 이미지 파일을 경로로 저장하는 방식
-    @Column(name = "profile_image_url")
-    private String profileImageUrl;
+    @Column(name = "profile_image")
+    private String profileImage;
     private String role;
 
     public MemberDto toDto() {
@@ -61,7 +61,7 @@ public class Member {
                 .nickname(this.nickname)
                 .lastLoginDate(this.lastLoginDate)
                 .userStatus(this.userStatus)
-                .profileImageUrl(this.profileImageUrl)
+                .profileImage(this.profileImage)
                 .role(this.role)
                 .build();
     }
