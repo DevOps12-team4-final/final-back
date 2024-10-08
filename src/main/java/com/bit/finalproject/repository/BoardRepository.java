31c3f0d5958a5483ed172b801f0be03d02ad0804
COM\planl <bobaesj20@naver.com>
2024-10-08 16:12:34 +0900

    24.10.08 feature/feed ㅇ상진 feed, user 이름 수정완

--- src/main/java/com/bit/finalproject/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bit.finalproject.repository;
-
-import com.bit.finalproject.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
