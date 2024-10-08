7ce0997b014001413ab871a1927bdf42735fe26e
sseopsseop <ponde119@outlook.com>
2024-10-08 18:30:44 +0900

    no message

---
+++ src/main/java/com/bit/finalproject/controller/BadgeController.java
@@ -0,0 +1,14 @@
+package com.bit.finalproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/badges")
+public class BadgeController {
+
+}
