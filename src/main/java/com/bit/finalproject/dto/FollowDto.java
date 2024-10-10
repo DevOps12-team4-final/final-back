b05cba4786f9a432e2ae193e04e8752a77461908
leeheeseng <red991018@gmail.com>
2024-10-10 14:23:31 +0900

    24.10.10 feature/member 이희승 팔로잉팔로우작업완료 멤버컨트롤러에추가 칭호작업중  어드민 페이지컨트롤러 진행중

---
+++ src/main/java/com/bit/finalproject/dto/FollowDto.java
@@ -0,0 +1,29 @@
+package com.bit.finalproject.dto;
+
+import com.bit.finalproject.entity.Follow;
+
+import com.bit.finalproject.entity.User;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class FollowDto {
+    private Long followId;        // 팔로우 ID
+    private Long followerId;      // 팔로워 ID
+    private Long followingId;     // 팔로잉 ID
+    private String followerName;  // 팔로워 이름 (추가적인 정보)
+    private String followingName; // 팔로잉 이름 (추가적인 정보)
+
+    // followDto를 엔티티로 변환하는 메서드
+    public Follow toEntity(User follower, User following) {
+        return Follow.builder()
+                .followId(this.followId)
+                .follower(follower)        // 팔로워 Member 객체
+                .following(following)      // 팔로잉 Member 객체
+                .build();
+    }
+}
