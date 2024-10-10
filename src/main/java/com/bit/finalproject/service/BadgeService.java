fc6f31cbf56a16edd232d0ecb821a1dd466ad90f
leeheeseng <red991018@gmail.com>
2024-10-10 18:02:34 +0900

    24.10.10 feature/member 이희승   칭호작업진행중 어드민이지컨트롤러 (예정)

--- src/main/java/com/bit/finalproject/service/BadgeService.java
+++ src/main/java/com/bit/finalproject/service/BadgeService.java
@@ -4,13 +4,12 @@ import com.bit.finalproject.entity.Badge;
 import com.bit.finalproject.entity.UserDetail;
 
 public interface BadgeService {
-    void checkConditionsForAllUsers();
 
-    void checkBadgeConditionsForUser(UserDetail user);
+
+
 
     boolean checkCondition(UserDetail user, Badge badge);
 
-    void awardBadgeToUser(UserDetail user, Badge badge);
 
     void checkAndRewardBadge(Long userId);
 }
--- src/main/java/com/bit/finalproject/service/impl/BadgeServiceImpl.java
+++ src/main/java/com/bit/finalproject/service/impl/BadgeServiceImpl.java
@@ -20,40 +20,18 @@ public class BadgeServiceImpl implements BadgeService {
 
     private final UserRepository userRepository;
     private final BadgeRepository badgeRepository;
-    @Override
-    @Transactional
-    public void checkConditionsForAllUsers() {
-        List<User> users = userRepository.findAll();
 
-        for (User user : users) {
-            checkBadgeConditionsForUser(user.getUserDetail());
-        }
-    }
-    @Override
-    public void checkBadgeConditionsForUser(UserDetail user) {
-        // 사용자별 배지 조건 확인 로직
-        List<Badge> badges = badgeRepository.findAll();
 
-        for (Badge badge : badges) {
-            boolean isConditionMet = checkCondition(user, badge);
 
-            if (isConditionMet) {
-                // 배지 지급 로직 (DB에 저장하거나 사용자에게 알림)
-                awardBadgeToUser(user, badge);
-            }
-        }
-    }
+
+
     @Override
     public boolean checkCondition(UserDetail user, Badge badge) {
         // 배지 조건 확인 로직 (운동 횟수, 도전 과제 달성 여부 등 체크)
         // 해당 로직은 Badge와 UserDetail의 상태에 따라 커스텀으로 작성 가능
         return true; // 예시로 true 반환
     }
-    @Override
-    public void awardBadgeToUser(UserDetail user, Badge badge) {
-        // 배지를 사용자에게 지급하는 로직
-        // UserBadge 테이블에 저장하거나 사용자에게 알림
-    }
+
     @Override
     public void checkAndRewardBadge(Long userId) {
         // 사용자에 대한 배지 조건을 체크하고, 조건이 충족되면 배지를 지급하는 로직을 구현
